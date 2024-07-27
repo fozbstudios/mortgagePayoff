af6282fb6a138d6ec5752ba2c54dae5f3d9421bc
Nick K <fozbstudios@gmail.com>
2024-07-27 15:49:27 -0500

    fixed typing errors

--- src/AmortizationChart.tsx
+++ src/AmortizationChart.tsx
@@ -1,16 +1,17 @@
-import { ResponsiveChartContainer, ChartsXAxis, ChartsAxisHighlight, ChartsReferenceLine, ChartsTooltip, ChartsYAxis, AxisConfig, axisClasses } from '@mui/x-charts'
+import { ResponsiveChartContainer, ChartsXAxis, ChartsAxisHighlight, ChartsReferenceLine, ChartsTooltip, ChartsYAxis, axisClasses } from '@mui/x-charts'
 import { AreaPlot, LineHighlightPlot, LinePlot, MarkPlot } from '@mui/x-charts/LineChart'
 import React from 'react'
 
 import { ChartsLegend } from "@mui/x-charts/ChartsLegend";
 import { breakpoints, currentBreakpoint } from './util/breakpoints';
-import { MonthlyMortgageStats as MonthlyMortgageStat } from './util/Calculations';
+// import { MonthlyMortgageStats as MonthlyMortgageStat } from './util/Calculations';
 import { currencyFormatter, paymentPortionFormatter, shortDateFormatter } from './util/Formatters';
+import { DatasetElementType } from '@mui/x-charts/internals';
 
 
 interface AmortizationChartProps {
   houseLandValue: number
-  monthlyMortgageStats:MonthlyMortgageStat[]
+  monthlyMortgageStats:DatasetElementType<number>[]
 }
 const ANGLE = 45
 
@@ -32,20 +33,6 @@ const AmortizationChart: React.FC<AmortizationChartProps> = ({
   }
   
 
-  const xAxis: [AxisConfig] = [{
-    id: "xaxis",
-    dataKey: 'date',
-    tickLabelInterval: (_value, index) => index % xAxisTickMultipes[currentBreakpoint()] === 0 || index + 1 === monthlyMortgageStats.length,
-    scaleType: 'band',
-    valueFormatter: (value, _context) => shortDateFormatter(value),
-    disableTicks: true,
-    tickLabelStyle: {
-      angle: ANGLE,
-      textAnchor: 'start',
-      fontSize: 12,
-    }
-  }]
-  const yAxis: [AxisConfig] = [{ label: "Balance", min: 0, max: houseLandValue + 10000 }];
 
   console.log('bp', breakpoints[currentBreakpoint()])
 
@@ -70,20 +57,29 @@ const AmortizationChart: React.FC<AmortizationChartProps> = ({
         }
       }
 
-      xAxis={xAxis}
-      yAxis={yAxis}
+      xAxis={[{ id: "dateAxis",
+        dataKey: 'date',
+        tickLabelInterval: (_value, index) => index % xAxisTickMultipes[currentBreakpoint()] === 0 || index + 1 === monthlyMortgageStats.length,
+        scaleType: 'band',
+        valueFormatter: (value) => shortDateFormatter(value),
+        disableTicks: true,
+        tickLabelStyle: {
+          angle: ANGLE,
+          textAnchor: 'start',
+          fontSize: 12,}}]}
+      yAxis={[{  id:'balanceAxis',  scaleType:'linear', label: "Balance", min: 0, max: houseLandValue + 10000 }]}
       dataset={monthlyMortgageStats}
       series={[
-        { type: 'line', label: 'Ammount Remaining($)', dataKey: 'newAmmountRemaining', id: 'newAmmountRemaining', showMark: ({ index }) => index % 12 === 0, valueFormatter: (v) => currencyFormatter.format(v) },
+        { type: 'line', label: 'Ammount Remaining($)', dataKey: 'newAmmountRemaining', id: 'newAmmountRemaining', showMark: ({ index }) => index % 12 === 0, valueFormatter: (v) => currencyFormatter.format(v as number ) },
         {
           type: 'line', label: 'Interest (% of Monthly Payment)', area: true, stack: '%s', showMark: false, disableHighlight: true, dataKey: 'interestHeightNomalized', id: 'interestHeightNomalized',
-          valueFormatter: (v, { dataIndex }) => {
+          valueFormatter: (_v, { dataIndex }) => {
             return paymentPortionFormatter(monthlyMortgageStats[dataIndex].interestAmmount, monthlyMortgageStats[dataIndex].interestPercent);
           },
         },
         {
           type: 'line', label: 'Principal (% of Monthly Payment)', area: true, stack: '%s', showMark: false, disableHighlight: true, dataKey: 'principalHeightNomalized', id: 'principalHeightNomalized',
-          valueFormatter: (v, { dataIndex }) => {
+          valueFormatter: (_v, { dataIndex }) => {
             return paymentPortionFormatter(monthlyMortgageStats[dataIndex].principalAmmount, monthlyMortgageStats[dataIndex].principalPercent);
           },
         },
